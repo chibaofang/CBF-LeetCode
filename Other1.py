b2e004bb995242a3e713be1b1c37fbe7bb998393
chibaofang <13474379185@163.com>
2015-06-22 23:58:02 +0800

    Update Other1.py

--- Other1.py
+++ Other1.py
@@ -1,1 +1,15 @@
 #计算字母出现的次数
+a = raw_input()
+a = a.lower()
+lst = list(a)
+d = {}
+
+for i in lst:
+    if i.isalpha():
+        if i in d:
+            continue
+        else:
+            d[i] = lst.count(i)
+    else:
+        continue
+print d
