393132e9836fc70b039e8f1512eaa8a7303f06bc
chibaofang <13474379185@163.com>
2015-06-23 13:38:42 +0800

    Update ZOJ1045.c

--- ZOJ1045.c
+++ ZOJ1045.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int card_no(float);
+int card_number(float);
 
 int main()
 {
@@ -8,13 +8,13 @@ int main()
   int cards = 0;
   printf("Please enther the length:\n");
   scanf("%f",&length);
-  cards = card_no(length);
+  cards = card_number(length);
   printf("%d card(s)\n", cards);
   
   return 0;
 }
 
-int card_no(float length){
+int card_number(float length){
   int n = 2;
   float length_sum = 0;
   float a = 0;
@@ -24,9 +24,9 @@ int card_no(float length){
   }else{
     while(length_sum < length){
       a = 1.00/n;  /*考察浮点数的运算规则*/
-      length_sum += a;
+      length_sum = length_sum + a;
       n++;
     }
-    return n-2;
   }
+  return n-2;
 }
